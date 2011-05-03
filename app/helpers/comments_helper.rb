d62a76639a9650f80e6ed0dcac4e9f82eb86c87d
Ing. Cristian Rasch <crasch@consejo.org.ar>
2011-05-03 18:49:42 -0300

    unused method deleted from CommentsHelper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,4 @@
 module CommentsHelper
-
   def new_comment_link(params)
     options = {:controller => :comments, :action => :new}
     value = params.find {|k,v| k.to_s =~ /(.+)_id$/}
@@ -12,9 +11,4 @@ module CommentsHelper
     options["#{commentable.class.name.downcase}_id".to_sym] = commentable.id
     link_to 'Comentarios', url_for(options)
   end
-  
-  def link_to_commentable(commentable)
-    link_to commentable.class.model_name.human.humanize, commentable
-  end
-
 end
