74df7af4ee2ab36d79d17ffba2c70c319da4591b
Ing. Cristian Rasch <crasch@consejo.org.ar>
2011-05-05 10:32:41 -0300

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ MethodologyApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => '130.0.200.160/methodology_app' }
 end
