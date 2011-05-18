de41295590cefdf4db1eb15c61f46e6bfbb1801a
Cristian Rasch <cristianrasch@gmail.com>
2011-05-18 18:07:48 -0300

    ..

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,17 @@ task :symlink_config_local_yml, :roles => :app do
 end
 after 'deploy:update_code', 'symlink_config_local_yml'
 
-# Delayed Job  
+# Delayed Job
+namespace(:delayed_job) do
+  desc 'Set env vars'
+  task :set_env, :roles => :app do
+    run "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/IBM/informix/lib:/opt/IBM/informix/lib/esql:/opt/IBM/informix/lib/cli:/usr/lib/sqlapi"
+    run "export INFORMIXSERVER=cpcecf_desar"
+  end
+end
+before "deploy:stop", "delayed_job:set_env"
+after  "deploy:start", "delayed_job:set_env"
+  
 before "deploy:restart", "delayed_job:stop"
 after  "deploy:restart", "delayed_job:start"
 
