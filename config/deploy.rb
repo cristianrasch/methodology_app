55fb2fe87abceaef98a6d8843d888ff54f4bde10
Cristian Rasch <cristianrasch@gmail.com>
2011-05-31 12:32:59 -0300

    ..

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,8 @@ require 'bundler/capistrano'
 require "delayed/recipes"
 
 # default_run_options[:pty] = true
-# default_environment['LD_LIBRARY_PATH'] = '/opt/IBM/informix/lib:/opt/IBM/informix/lib/esql:/opt/IBM/informix/lib/cli:/usr/lib/sqlapi' 
-# default_environment['INFORMIXSERVER'] = 'cpcecf_desar'
+default_environment['LD_LIBRARY_PATH'] = '/opt/IBM/informix/lib:/opt/IBM/informix/lib/esql:/opt/IBM/informix/lib/cli:/usr/lib/sqlapi' 
+default_environment['INFORMIXSERVER'] = 'cpcecf_desar'
 
 set :application, 'methodology_app'
 set :rails_env, "production" #added for delayed job 
