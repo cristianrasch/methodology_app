334b491ffd7d8fb27b21f69ac0ff7490e9b03f93
Cristian Rasch <cristianrasch@gmail.com>
2011-06-13 17:24:35 -0300

    ..

--- lib/duration.rb
+++ lib/duration.rb
@@ -6,9 +6,8 @@ module Duration
   def in_days(obj, attr)
     case(obj.send("#{attr}_unit"))
       when HOUR then (obj.send(attr) > 4 ? 1 : 0)*6
-      when DAY then obj.send(attr)
+      when DAY then obj.send(attr).to_i
       when WEEK then obj.send(attr)*5
-      else 0
     end
   end
 end
